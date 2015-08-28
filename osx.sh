e1bcb3a35404bcad46f2c069e7d04a347da9e61d
Justin Shrake <justinshrake@gmail.com>
2015-08-28 07:49:18 -0700

    Better error checking and cleanup

--- osx.sh
+++ osx.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-
+# This script will acquire the hearthstone-json binary for your platform
+# and extract
 set -e
+# set -v
 
+# Disunity requires java! Sorry!
 if ! type -p java >> /dev/null; then
-  echo "Cannot find a java installation on this computer"
-  echo "This script uses disunity, a java program to extract the XML card definitions";
+  echo "Error: this script requires java for disunity, a program to extract assets from unity archive files";
   exit 1;
 fi
 
@@ -14,26 +16,40 @@ if [ -z $PATH_TO_CARDDEF ];
   then
   PATH_TO_CARDDEF="/Applications/Hearthstone/Data/OSX/cardxml0.unity3d"
 fi
-echo "Path to cardxml0.unity3d file: ${PATH_TO_CARDDEF}"
-cp ${PATH_TO_CARDDEF} carddefs.unity3d
+
+if [ ! -f ${PATH_TO_CARDDEF} ]
+  then
+  echo "File not found: ${PATH_TO_CARDDEF}"
+  echo "Please provide a path to your Hearthstone cardxml0.unity3d file"
+  exit 1
+fi
+
+# Download the appropriate hearthstone-json binary
+ARCH=`uname -m`
+HEARTHSTONE_JSON="hearthstone-json-${ARCH}-macosx"
+# TODO(jshrake): Acquire binary from Release binaries
 
 # Use disunity to extract the card def xml files and transform each to json
-# Acquire disunity
 DISUNITY_VERS=0.3.4
-echo "Acquiring disunity_v${DISUNITY_VERS}"
 curl -LOk https://github.com/ata4/disunity/releases/download/v${DISUNITY_VERS}/disunity_v${DISUNITY_VERS}.zip
 tar -xzvf disunity_v${DISUNITY_VERS}.zip disunity.jar lib
-java -jar disunity.jar extract carddefs.unity3d
+cp ${PATH_TO_CARDDEF} cardxml0.unity3d
+java -jar disunity.jar extract cardxml0.unity3d
+rm cardxml0.unity3d
+
 # Transform the xml files
 mkdir -p output
-for file in carddefs/CAB-cardxml0/TextAsset/*.txt; do
+for file in cardxml0/CAB-cardxml0/TextAsset/*.txt; do
   filename=$(basename "$file")
   extension="${filename##*.}"
   filename="${filename%.*}"
-  echo "Converting $file to output/${filename}.json";
-  ./hearthstone-json ${file} > output/${filename}.json;
+  output="output/${filename}.json"
+  echo "Generating ${output} from ${file}";
+  ./hearthstone-json ${file} > ${output};
 done
+echo "Finished! Please find the JSON files in the output directory"
+
 # Clean up
 rm -rf disunity*
-rm -rf carddefs*
+rm -rf cardxml0*
 rm -rf lib
