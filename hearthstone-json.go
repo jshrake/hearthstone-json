fec5c3f3421b7b999801fdc03eb28281624f1431
Justin Shrake <justinshrake@gmail.com>
2015-08-29 10:56:07 -0700

    cleanup: use global variables for the conversion maps

--- hearthstone-json.go
+++ hearthstone-json.go
@@ -28,162 +28,146 @@ type CardDefs struct {
 }
 
 // Conversion tables, credit to https://github.com/Sembiance/hearthstonejson
-func EnumIDToString() map[int]string {
-	return map[int]string{
-		185: "CardName",
-		183: "CardSet",
-		202: "CardType",
-		201: "Faction",
-		199: "Class",
-		203: "Rarity",
-		48:  "Cost",
-		251: "AttackVisualType",
-		184: "CardTextInHand",
-		47:  "Atk",
-		45:  "Health",
-		321: "Collectible",
-		342: "ArtistName",
-		351: "FlavorText",
-		32:  "TriggerVisual",
-		330: "EnchantmentBirthVisual",
-		331: "EnchantmentIdleVisual",
-		268: "DevState",
-		365: "HowToGetThisGoldCard",
-		190: "Taunt",
-		364: "HowToGetThisCard",
-		338: "OneTurnEffect",
-		293: "Morph",
-		208: "Freeze",
-		252: "CardTextInPlay",
-		325: "TargetingArrowText",
-		189: "Windfury",
-		218: "Battlecry",
-		200: "Race",
-		192: "Spellpower",
-		187: "Durability",
-		197: "Charge",
-		362: "Aura",
-		361: "HealTarget",
-		349: "ImmuneToSpellpower",
-		194: "Divine Shield",
-		350: "AdjacentBuff",
-		217: "Deathrattle",
-		191: "Stealth",
-		220: "Combo",
-		339: "Silence",
-		212: "Enrage",
-		370: "AffectedBySpellPower",
-		240: "Cant Be Damaged",
-		114: "Elite",
-		219: "Secret",
-		363: "Poisonous",
-		215: "Recall",
-		340: "Counter",
-		205: "Summoned",
-		367: "AIMustPlay",
-		335: "InvisibleDeathrattle",
-		377: "UKNOWN_HasOnDrawEffect",
-		388: "SparePart",
-		389: "UNKNOWN_DuneMaulShaman",
-		380: "UNKNOWN_Blackrock_Heroes",
-		396: "UNKNOWN_Grand_Tournement_Fallen_Hero",
-		401: "UNKNOWN_BroodAffliction",
-		402: "UNKNOWN_Intense_Gaze",
-		403: "Inspire",
-		404: "UNKNOWN_Grand_Tournament_Arcane_Blast",
-	}
+var EnumIDToString = map[int]string{
+	185: "CardName",
+	183: "CardSet",
+	202: "CardType",
+	201: "Faction",
+	199: "Class",
+	203: "Rarity",
+	48:  "Cost",
+	251: "AttackVisualType",
+	184: "CardTextInHand",
+	47:  "Atk",
+	45:  "Health",
+	321: "Collectible",
+	342: "ArtistName",
+	351: "FlavorText",
+	32:  "TriggerVisual",
+	330: "EnchantmentBirthVisual",
+	331: "EnchantmentIdleVisual",
+	268: "DevState",
+	365: "HowToGetThisGoldCard",
+	190: "Taunt",
+	364: "HowToGetThisCard",
+	338: "OneTurnEffect",
+	293: "Morph",
+	208: "Freeze",
+	252: "CardTextInPlay",
+	325: "TargetingArrowText",
+	189: "Windfury",
+	218: "Battlecry",
+	200: "Race",
+	192: "Spellpower",
+	187: "Durability",
+	197: "Charge",
+	362: "Aura",
+	361: "HealTarget",
+	349: "ImmuneToSpellpower",
+	194: "Divine Shield",
+	350: "AdjacentBuff",
+	217: "Deathrattle",
+	191: "Stealth",
+	220: "Combo",
+	339: "Silence",
+	212: "Enrage",
+	370: "AffectedBySpellPower",
+	240: "Cant Be Damaged",
+	114: "Elite",
+	219: "Secret",
+	363: "Poisonous",
+	215: "Recall",
+	340: "Counter",
+	205: "Summoned",
+	367: "AIMustPlay",
+	335: "InvisibleDeathrattle",
+	377: "UKNOWN_HasOnDrawEffect",
+	388: "SparePart",
+	389: "UNKNOWN_DuneMaulShaman",
+	380: "UNKNOWN_Blackrock_Heroes",
+	396: "UNKNOWN_Grand_Tournement_Fallen_Hero",
+	401: "UNKNOWN_BroodAffliction",
+	402: "UNKNOWN_Intense_Gaze",
+	403: "Inspire",
+	404: "UNKNOWN_Grand_Tournament_Arcane_Blast",
 }
 
-func CardSetIDToString() map[int]string {
-	return map[int]string{
-		2:  "Basic",
-		3:  "Classic",
-		4:  "Reward",
-		5:  "Missions",
-		7:  "System",
-		8:  "Debug",
-		11: "Promotion",
-		12: "Curse of Naxxramas",
-		13: "Goblins vs Gnomes",
-		14: "Blackrock Mountain",
-		15: "The Grand Tournament",
-		16: "Credits",
-		17: "Hero Skins",
-		18: "Tavern Brawl",
-	}
+var CardSetIDToString = map[int]string{
+	2:  "Basic",
+	3:  "Classic",
+	4:  "Reward",
+	5:  "Missions",
+	7:  "System",
+	8:  "Debug",
+	11: "Promotion",
+	12: "Curse of Naxxramas",
+	13: "Goblins vs Gnomes",
+	14: "Blackrock Mountain",
+	15: "The Grand Tournament",
+	16: "Credits",
+	17: "Hero Skins",
+	18: "Tavern Brawl",
 }
 
-func CardTypeIDToString() map[int]string {
-	return map[int]string{
-		3:  "Hero",
-		4:  "Minion",
-		5:  "Spell",
-		6:  "Enchantment",
-		7:  "Weapon",
-		10: "Hero Power",
-	}
+var CardTypeIDToString = map[int]string{
+	3:  "Hero",
+	4:  "Minion",
+	5:  "Spell",
+	6:  "Enchantment",
+	7:  "Weapon",
+	10: "Hero Power",
 }
 
-func RarityIDToString() map[int]string {
-	return map[int]string{
-		0: "undefined",
-		1: "Common",
-		2: "Free",
-		3: "Rare",
-		4: "Epic",
-		5: "Legendary",
-	}
+var RarityIDToString = map[int]string{
+	0: "undefined",
+	1: "Common",
+	2: "Free",
+	3: "Rare",
+	4: "Epic",
+	5: "Legendary",
 }
 
-func CardRaceIDToString() map[int]string {
-	return map[int]string{
-		14: "Murloc",
-		15: "Demon",
-		20: "Beast",
-		21: "Totem",
-		23: "Pirate",
-		24: "Dragon",
-		17: "Mech",
-	}
+var CardRaceIDToString = map[int]string{
+	14: "Murloc",
+	15: "Demon",
+	20: "Beast",
+	21: "Totem",
+	23: "Pirate",
+	24: "Dragon",
+	17: "Mech",
 }
 
-func ClassIDToString() map[int]string {
-	return map[int]string{
-		0:  "undefined",
-		2:  "Druid",
-		3:  "Hunter",
-		4:  "Mage",
-		5:  "Paladin",
-		6:  "Priest",
-		7:  "Rogue",
-		8:  "Shaman",
-		9:  "Warlock",
-		10: "Warrior",
-		11: "Dream",
-	}
+var ClassIDToString = map[int]string{
+	0:  "undefined",
+	2:  "Druid",
+	3:  "Hunter",
+	4:  "Mage",
+	5:  "Paladin",
+	6:  "Priest",
+	7:  "Rogue",
+	8:  "Shaman",
+	9:  "Warlock",
+	10: "Warrior",
+	11: "Dream",
 }
 
-func FactionIDToString() map[int]string {
-	return map[int]string{
-		1: "Horde",
-		2: "Alliance",
-		3: "Neutral",
-	}
+var FactionIDToString = map[int]string{
+	1: "Horde",
+	2: "Alliance",
+	3: "Neutral",
 }
 
-func Mechanics() []string {
-	return []string{
-		"Windfury", "Combo", "Secret", "Battlecry", "Deathrattle",
-		"Taunt", "Stealth", "Spellpower", "Enrage", "Freeze",
-		"Charge", "Overload", "Divine Shield", "Silence", "Morph",
-		"OneTurnEffect", "Poisonous", "Aura", "AdjacentBuff",
-		"HealTarget", "GrantCharge", "ImmuneToSpellpower",
-		"AffectedBySpellPower", "Summoned", "Inspire",
-	}
+var Mechanics = []string{
+	"Windfury", "Combo", "Secret", "Battlecry", "Deathrattle",
+	"Taunt", "Stealth", "Spellpower", "Enrage", "Freeze",
+	"Charge", "Overload", "Divine Shield", "Silence", "Morph",
+	"OneTurnEffect", "Poisonous", "Aura", "AdjacentBuff",
+	"HealTarget", "GrantCharge", "ImmuneToSpellpower",
+	"AffectedBySpellPower", "Summoned", "Inspire",
 }
 
 func IsMechanic(s string) bool {
-	for _, mechanic := range Mechanics() {
+	for _, mechanic := range Mechanics {
 		if strings.ToLower(mechanic) == strings.ToLower(s) {
 			return true
 		}
@@ -220,22 +204,22 @@ func EntityToJson(e Entity) JsonCard {
 	var card JsonCard
 	card.ID = e.CardID
 	for _, tag := range e.Tags {
-		enum := EnumIDToString()[tag.EnumID]
+		enum := EnumIDToString[tag.EnumID]
 		switch {
 		case enum == "CardName":
 			card.Name = tag.StringValue
 		case enum == "CardSet":
-			card.Set = CardSetIDToString()[tag.Value]
+			card.Set = CardSetIDToString[tag.Value]
 		case enum == "CardType":
-			card.Type = CardTypeIDToString()[tag.Value]
+			card.Type = CardTypeIDToString[tag.Value]
 		case enum == "CardRace":
-			card.Race = CardRaceIDToString()[tag.Value]
+			card.Race = CardRaceIDToString[tag.Value]
 		case enum == "Class":
-			card.Class = ClassIDToString()[tag.Value]
+			card.Class = ClassIDToString[tag.Value]
 		case enum == "Rarity":
-			card.Rarity = RarityIDToString()[tag.Value]
+			card.Rarity = RarityIDToString[tag.Value]
 		case enum == "Faction":
-			card.Faction = FactionIDToString()[tag.Value]
+			card.Faction = FactionIDToString[tag.Value]
 		case enum == "Atk":
 			card.Attack = tag.Value
 		case enum == "Health":
